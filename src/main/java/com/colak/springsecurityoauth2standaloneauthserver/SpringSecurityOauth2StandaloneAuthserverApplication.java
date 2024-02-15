005ff218d7bfda0d7dbe43dc8ce0d88fcb3ca1ee
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-15 12:48:25 +0300

    Initial commit

---
+++ src/main/java/com/colak/springsecurityoauth2standaloneauthserver/SpringSecurityOauth2StandaloneAuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.colak.springsecurityoauth2standaloneauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2StandaloneAuthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauth2StandaloneAuthserverApplication.class, args);
+	}
+
+}
